4d4e3c18242f5a0a7786645af3918ca190f96561
yuoku <appledev.yuoku@gmail.com>
2021-12-01 00:28:40 +0900

    added AccordionListViewController.

---
+++ SwiftAccordion/Presentation/AccordionList/View/AccordionListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccordionListViewController.swift
+//  SwiftAccordion
+//
+//  Created by Yuki Okudera on 2021/12/01.
+//
+
+import UIKit
+
+class AccordionListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
